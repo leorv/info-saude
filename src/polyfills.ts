103165f04a23f0587895d155f5707e8238686f6c
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-06-04 18:21:12 -0300

    update angular

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -41,7 +41,11 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+(window as any).__zone_symbol__UNPATCHED_EVENTS =['scroll', 'mousemove']; // disable patch specified eventNames
 
+(window as any).__Zone_enable_cross_context_check = true;
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
